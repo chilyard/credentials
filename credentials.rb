2e393dec95c08400fe3ca253094d7f0a87de20ad
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-06-29 15:20:02 -0700

    updates

--- credentials.rb
+++ credentials.rb
@@ -5,11 +5,12 @@ module RLCredentials
 	def loadbalancer(*args)
 	  print "you passed: ", args[0], "\n"
 	  print "loading netscaler api credentials\n"
-	  creds_file = File.open('/home/chilyard/projects/rl_credentials/netscaler_apicreds.json', "rb")
+	  creds_file = File.open('../rl_credentials/netscaler_apicreds.json', "rb")
 	  content = creds_file.read
 	  creds_hash = JSON.parse(content)
-	  print "username: ", creds_hash['login']['username'], "\n"
-	  print "password: ", creds_hash['login']['password'], "\n"
+	  username = creds_hash['login']['username']
+	  password = creds_hash['login']['password']
+      return username, password
 	end
 
 end
