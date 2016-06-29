8d073220aff375e7d531cb71a898e60cd393d2bd
chilyard <chilyard@example.com>
2016-06-28 21:23:26 -0700

    cleaned up spacing

--- credentials.rb
+++ credentials.rb
@@ -4,12 +4,12 @@ module RLCredentials
 
 	def loadbalancer(*args)
 	  print "you passed: ", args[0], "\n"
-      print "loading netscaler api credentials\n"
-      creds_file = File.open('/home/chilyard/projects/rl_credentials/netscaler_apicreds.json', "rb")
-      content = creds_file.read
-      creds_hash = JSON.parse(content)
-      print "username: ", creds_hash['login']['username'], "\n"
-      print "password: ", creds_hash['login']['password'], "\n"
+	  print "loading netscaler api credentials\n"
+	  creds_file = File.open('/home/chilyard/projects/rl_credentials/netscaler_apicreds.json', "rb")
+	  content = creds_file.read
+	  creds_hash = JSON.parse(content)
+	  print "username: ", creds_hash['login']['username'], "\n"
+	  print "password: ", creds_hash['login']['password'], "\n"
 	end
 
 end
