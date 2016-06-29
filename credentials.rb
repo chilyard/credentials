fae2b3774d8530ceced08d4b9c7f2a394b6d7468
chilyard <chilyard@example.com>
2016-06-28 21:21:49 -0700

    bla hblah

--- credentials.rb
+++ credentials.rb
@@ -2,14 +2,10 @@
 
 module RLCredentials
 
-	def intialize
-	  puts "RLCredentials init"
-	end
-
-
-	def LoadBalancer
+	def loadbalancer(*args)
+	  print "you passed: ", args[0], "\n"
       print "loading netscaler api credentials\n"
-      creds_file = File.open('netscaler_apicreds.json', "rb")
+      creds_file = File.open('/home/chilyard/projects/rl_credentials/netscaler_apicreds.json', "rb")
       content = creds_file.read
       creds_hash = JSON.parse(content)
       print "username: ", creds_hash['login']['username'], "\n"
